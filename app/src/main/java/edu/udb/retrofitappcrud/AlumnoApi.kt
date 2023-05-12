80dec77d9833b20f8f5be3f3a25d2561d995e535
Alexis Cornejo <53987686+alexis1711@users.noreply.github.com>
2023-05-12 00:52:13 -0600

    Proyecto Inicio

---
+++ app/src/main/java/edu/udb/retrofitappcrud/AlumnoApi.kt
@@ -0,0 +1,22 @@
+package edu.udb.retrofitappcrud
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface AlumnoApi {
+
+    @GET("alumnos")
+    fun obtenerAlumnos(): Call<List<Alumno>>
+
+    @GET("alumnos/{id}")
+    fun obtenerAlumnoPorId(@Path("id") id: Int): Call<Alumno>
+
+    @POST("alumnos")
+    fun crearAlumno(@Body alumno: Alumno): Call<Alumno>
+
+    @PUT("alumnos/{id}")
+    fun actualizarAlumno(@Path("id") id: Int, @Body alumno: Alumno): Call<Alumno>
+
+    @PUT("alumnos/{id}")
+    fun eliminarAlumno(@Path("id") id: Int, @Body alumno: Alumno): Call<Void>
+}
